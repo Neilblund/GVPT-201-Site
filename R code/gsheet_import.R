176a4f449dc935610f296b680015ae705936dded
Neilblund <neilblund@gmail.com>
2025-02-03 12:17:48 -0500

    Add

--- R code/gsheet_import.R
+++ R code/gsheet_import.R
@@ -1,31 +1,32 @@
 # This section is just installing a package and importing the data
 packages<-installed.packages()
+needed_packages<-c('labelled', 'googlesheets4')
 
-if(!'googlesheets4' %in% packages[,1]){
-  install.packages('googlesheets4')
+needed_packages<-needed_packages[which(needed_packages%in%packages[,1] == FALSE)]
+for(i in needed_packages){
+  install.packages(needed_packages)
 }
 library(googlesheets4)
+library(labelled)
 url<-'https://docs.google.com/spreadsheets/d/1ThOq7SQjnOQxvdbzUawE5jo5UwOyoSSDxDcPMec5C8Q/edit?gid=518794249#gid=518794249'
 responses<-read_sheet(url, sheet='Responses')
 newnames<-read_sheet(url, sheet='names')
 colnames(responses) <- newnames$colname
 # adding labels
+var_label(responses) <- newnames$label
+
 for(i in colnames(responses)){
   index<-which(newnames$colname==i)
-  labs<-newnames$label[index]
-  responses[[i]] <- haven::labelled(responses[[i]], 
-                                    label=labs)
   if(!is.na(newnames$labels[index])){
     labels<-eval(parse(text=newnames$labels[index]))
     responses[[i]] <- factor(responses[[i]], levels=labels)
     
-  }else{
-    responses[[i]] <- haven::labelled(responses[[i]], 
-                                      label=labs)
   }
   
-  
 }
+
+
+
 
 saveRDS(responses, file='responses.rds')
 
